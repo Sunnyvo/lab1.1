e9b8ec869d5dc651a86dc26bf9739a2978b4d08b
sunnyvo <votanphu.2101@gmail.com>
2017-11-23 17:33:08 +0700

    first commit

--- instalclone/src/InstaList.js
+++ instalclone/src/InstaList.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import InstaPost from "./InstaPost"
   export default class InstaList extends React.Component {
+		componentWillMount() {
+			const key = 'token';
+			console.log('mounted');
+			const existingToken = sessionStorage.getItem(key);
+			console.log(existingToken);
+			const accessToken = window.location.hash.split("=")[1];
+			console.log(accessToken);
+			const oauthUrl = "https://www.instagram.com/oauth/authorize/?client_id=0823ada4b2394de49d191f727e521837&redirect_uri=http://localhost:3000&response_type=token";
+
+			if (!accessToken && !existingToken) {
+				window.location.replace(oauthUrl);
+			}
+		}
     render() {
 			const posts = this.props.posts && this.props.posts.map((post) => {
 				return (<InstaPost {...post}/>);
